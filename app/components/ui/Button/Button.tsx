82f75ff1f671cf8a419b1c39920e2d00d03c76f1
LaberSor <vlad.shumkin.rc@gmail.com>
2023-08-02 09:26:16 +0300

    Add icons, update login form

--- app/components/ui/Button/Button.tsx
+++ app/components/ui/Button/Button.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import './styles.css'
+import React, { FC, ButtonHTMLAttributes } from 'react'
+import './styles.scss'
 import classnames from 'classnames'
+import googlelogo from '../../../images/google-logo.png'
 
-function Button({ label, ...props }) {
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string
+  icon?: string
+  props: string[]
+}
+
+function Button({ label, icon, ...props }: IButton) {
+  const buttonClassnames = classnames(
+    props.className,
+    'button-wrapper rounded-lg',
+    icon ? 'default border border-slate-300 font-semibold' : 'custom',
+  )
   return (
-    <button
-      className={classnames(props.className, 'button-wrapper rounded-lg')}
-    >
-      {label}
+    <button className={buttonClassnames} type="button">
+      {/* {icon && <img src={googlelogo} className="pr-1" />} */}
+      {label && <span className="">{label}</span>}
     </button>
   )
 }
--- app/components/ui/Input/Input.tsx
+++ app/components/ui/Input/Input.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useMemo, InputHTMLAttributes } from 'react'
 import classnames from 'classnames'
 import './styles.css'
 
-function Input({ label, ...props }) {
+interface IInput extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
+  field?: object
+}
+
+function Input({ label, field, name, ...props }: IInput) {
+  const fieldObject = useMemo(() => {
+    if (field) {
+      return field
+    }
+
+    return {}
+  }, [field])
+
   return (
     <div
       className={classnames(props.className, 'wrapper container flex flex-col')}
     >
       {label && <label className="label font-semibold py-1">{label}</label>}
       <input
-        type="text"
+        type={props.type || 'text'}
         className="p-2 rounded-xs rounded-md border-2 border-slate-300"
+        {...fieldObject}
       />
     </div>
   )
